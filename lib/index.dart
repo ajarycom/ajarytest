f06e5c808d77ac6aa42142a544cfd1fcf1662ed5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-28 15:02:05 +0000

    Updating to the latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,2 @@
+// Export pages
+export '/pages/unlock_page/unlock_page_widget.dart' show UnlockPageWidget;
---
+++ lib/pages/unlock_page/unlock_page_widget.dart
@@ -0,0 +1,81 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'unlock_page_model.dart';
+export 'unlock_page_model.dart';
+
+class UnlockPageWidget extends StatefulWidget {
+  const UnlockPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _UnlockPageWidgetState createState() => _UnlockPageWidgetState();
+}
+
+class _UnlockPageWidgetState extends State<UnlockPageWidget> {
+  late UnlockPageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => UnlockPageModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: SafeArea(
+          top: true,
+          child: Align(
+            alignment: AlignmentDirectional(1.00, 0.00),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FFButtonWidget(
+                  onPressed: () {
+                    print('ButtonUnlock pressed ...');
+                  },
+                  text: 'Unlock',
+                  options: FFButtonOptions(
+                    height: 40.0,
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                    iconPadding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                    color: FlutterFlowTheme.of(context).primary,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Readex Pro',
+                          color: Colors.white,
+                        ),
+                    elevation: 3.0,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
